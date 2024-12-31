3d7b24e46c36c1be23a492fb0af74632e82a62a1
unknown <keshav.wanjale@gmail.com>
2024-12-31 14:46:35 +0530

    [Keshav] FEAT: CORS CONFIG

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -51,10 +51,14 @@ INSTALLED_APPS = [
     'user',
     'book',
     'cart',
+
+    'corsheaders',
     
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -222,3 +226,13 @@ SWAGGER_SETTINGS = {
    },
    'USE_SESSION_AUTH': False
 }
+
+# CORS Settings
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React app origin
+    "http://127.0.0.1:3000",  # Alternative localhost origin
+]
+
+CORS_ALLOW_CREDENTIALS = True
